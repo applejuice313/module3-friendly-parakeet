b388523b792d2680cc5d6d6de812d4cab9935b74
applejuice313 <44757000+applejuice313@users.noreply.github.com>
2021-10-01 15:35:48 -0600

    added user prompt to enter password length

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,34 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("you clicked the button!");
+
+// 1. prompt the user for password criteria
+//    a. password from 8 through 128 characters
+  var passwordLength
+    passwordLength = prompt("How many characters would you like your password to contain? Please type a value from 8 to 128 characters.");
+}
+//  if (passwordLength >=8 && passwordLength <=128) {
+//    console.log("valid input entered");
+//  }
+//
+//  else if (passwordLength )
+
+   // else (passwordLength <8 && passwordLength >128) 
+  //  console.log("invalid password length");
+  //  }
+
+  
+//    b. lowercase, upercase, numbers, special characters
+// 2. validate input
+// 3. generate password
+
+
+// 4. display password on page
+  // return "generated password will go here";
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
